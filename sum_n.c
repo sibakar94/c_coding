2aed20fb1f8c59a27f47cc532793c2a1e3762148
Siba Kar <47991124+sibakar94@users.noreply.github.com>
2019-04-03 20:33:28 +0530

    Update sum_n.c

--- sum_n.c
+++ sum_n.c
@@ -5,7 +5,11 @@ int main()
     int num,sum;
     printf("enter N");
     scanf("%d",&num);
+    if(num<0)
+        printf("Invalid");
+    else{
     sum=num*(num+1)/2;
     printf("%d",sum);
-        
+    }
 }
+
