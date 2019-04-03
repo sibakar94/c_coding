c32bf90758f5d99898ba68223f887bf09d5c7ef8
Siba Kar <47991124+sibakar94@users.noreply.github.com>
2019-04-03 21:27:11 +0530

    Update odd_interval.c

--- odd_interval.c
+++ odd_interval.c
@@ -5,7 +5,11 @@ int main()
     int num1,num2;
     printf("enter two intervals");
     scanf("%d%d",&num1,&num2);
+    if(num1<0 ||num2<0)
+        printf("Invalid");
+    else{
     for(int i=num1+1;i<num2;i++)
         if(i%2!=0)
             printf("%d",i);
+    }
 }
