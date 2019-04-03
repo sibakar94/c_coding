652ccca9f64c9ed8f81acf1cd256eeaf021784ff
Siba Kar <47991124+sibakar94@users.noreply.github.com>
2019-04-03 21:06:54 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int num,i,c=0;
+    printf("enter a number");
+    scanf("%d",&num);
+    for(i=2;i<num;i++)
+        if(num%i==0){
+        c=1;
+        break;}
+    if(c==1)
+        printf("No");
+    else
+        printf("Yes");
+}
