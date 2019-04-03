54210c2db17deaf2248541cc8a34486397adcd23
Siba Kar <47991124+sibakar94@users.noreply.github.com>
2019-04-03 21:11:37 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -2,7 +2,7 @@
 int main()
 {
     int num,i,c=0;
-    printf("enter a number");
+    printf("enter a number less than equals to 1000");
     scanf("%d",&num);
     for(i=2;i<num;i++)
         if(num%i==0){
