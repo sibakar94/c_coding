fb20d22dc1dd0afe5a09fa8572e68d0425ec1076
Siba Kar <47991124+sibakar94@users.noreply.github.com>
2019-04-03 20:09:52 +0530

    Create largest_num.c

---
+++ largest_num.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1,num2,num3,large;
+    printf("enter 3 numbers");
+    scanf("%d%d%d",&num1,&num2,&num3);
+    if(num1>num2 && num1>num3)
+        large=num1;
+    else if(num2>num3)
+        large=num2;
+    else
+        large=num3;
+    printf("%d",num3);
+}
