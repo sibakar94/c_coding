d698a844830cbb14ae12e57267a7137129f224d3
Siba Kar <47991124+sibakar94@users.noreply.github.com>
2019-04-04 11:58:26 +0530

    Create camel_case.c

---
+++ camel_case.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main()
+{
+    char str[20];
+    printf("enter a string");
+    scanf("%s",str);
+    int len=strlen(str);
+    str[0]=toupper(str[0]);
+    for(int i=1;i<len;i++)
+    {
+        if(str[i]==' '){
+            str[i+1]=toupper(str[i+1]);
+            i++;
+            continue;
+        }
+        else
+        str[i]=tolower(str[i]);
+    }
+    
+    printf("%s",str);
+    
+}
