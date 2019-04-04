a003fd00c7924a8a83f9fa7d17d41311f17a71a4
Siba Kar <47991124+sibakar94@users.noreply.github.com>
2019-04-04 15:37:10 +0530

    Update distinct_num.c

--- distinct_num.c
+++ distinct_num.c
@@ -1,11 +1,16 @@
-int main() {
-  int len,result;
+#include <stdio.h>
+
+int main()
+{
+    int len,result;
   printf("enter the size of integer array");
   scanf("%d",&len);
   int arr[len];
+  
   printf("enter the array");
   for(int i=0;i<len;i++)
-    scanf("%d",&arr[i])
+    scanf("%d",&arr[i]);
+  
   for(int i=0;i<len;i++){
   	int count=0;
     for(int j=0;j<len;j++){
