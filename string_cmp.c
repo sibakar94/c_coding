553d29f41639ce21648cea8741695da35bf002a1
Siba Kar <47991124+sibakar94@users.noreply.github.com>
2019-04-04 12:33:27 +0530

    Update string_cmp.c

--- string_cmp.c
+++ string_cmp.c
@@ -5,15 +5,23 @@
 int main()
 {
     char str1[10],str2[10];
-    int result;
+    int equal=0,len1,len2;
     printf("enter two string");
     scanf("%s%s",&str1,&str2);
-    result=strcmp(str1,str2);
-    if(result==-1 ||result==1){
-        printf("Yes");
-    }
-    else
+    len1=strlen(str1);
+    len2=strlen(str2);
+    if(len1!=len2)
         printf("No");
-        
+    else{
+        for(int i=0;i<len1;i++){
+            if(str1[i]==str2[i])
+                equal++;
+        }
+        if(len1-equal==1){
+            printf("Yes");
+        }
+        else
+            printf("No");
+    }
     
 }
